f34943cb09d8c45f5ece92380d809baeb66701ae
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-08-01 17:05:04 +0100

    sidebar eleent for desktop

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,11 +4,64 @@ import Link from 'next/link';
 import React from 'react';
 import { Montserrat } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import {
+  LayoutDashboard,
+  MessageSquare,
+  ImageIcon,
+  VideoIcon,
+  Music,
+  Code,
+  Settings,
+} from 'lucide-react';
 
 const monserrat = Montserrat({
   weights: [600],
   subsets: ['latin'],
 });
+
+const routes = [
+  {
+    label: 'Dashboard',
+    icon: LayoutDashboard,
+    href: '/dashboard',
+    color: 'text-sky-500',
+  },
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: 'text-violet-500',
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    href: '/image',
+    color: 'text-pink-500',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    href: '/video',
+    color: 'text-orange-700',
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    href: '/music',
+    color: 'text-emerald-500',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    href: '/code',
+    color: 'text-green-700',
+  },
+  {
+    label: 'Settings',
+    icon: Settings,
+    href: '/settings',
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -23,6 +76,23 @@ const Sidebar = () => {
             Genius
           </h1>
         </Link>
+
+        <div className='space-y-1'>
+          {routes.map((route) => (
+            <Link key={route.href} href={route.href}>
+              <div className='flex items-center gap-2 flex-1 hover:bg-[#1f2937]  space-x-4 px-3 py-4 rounded-md'>
+                <route.icon
+                  className={cn(
+                    'h-6 w-6 transition-all duration-500 ',
+                    route.color
+                  )}
+                  size={20}
+                />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
