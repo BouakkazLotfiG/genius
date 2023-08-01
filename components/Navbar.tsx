d3dacc0d11c59b79ac96f523406ba67d00972988
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-08-01 18:09:17 +0100

    dashboard content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,12 @@
 import { UserButton } from '@clerk/nextjs';
-import { Menu } from 'lucide-react';
+
 import React from 'react';
-import { Button } from './ui/button';
+import MobileSidebar from './MobileSidebar';
 
 const Navbar = () => {
   return (
     <div className='flex items-center p-4'>
-      <Button variant='ghost' size='icon' className='md:hidden'>
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className='flex w-full justify-end '>
         <UserButton afterSignOutUrl='/' />
       </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,6 +13,7 @@ import {
   Code,
   Settings,
 } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 const monserrat = Montserrat({
   weights: [600],
@@ -64,6 +65,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className='space-y-4 flex flex-col h-full bg-[#111827] text-white'>
       {' '}
@@ -79,15 +81,18 @@ const Sidebar = () => {
 
         <div className='space-y-1'>
           {routes.map((route) => (
-            <Link key={route.href} href={route.href}>
-              <div className='flex items-center gap-2 flex-1 hover:bg-[#1f2937]  space-x-4 px-3 py-4 rounded-md'>
-                <route.icon
-                  className={cn(
-                    'h-6 w-6 transition-all duration-500 ',
-                    route.color
-                  )}
-                  size={20}
-                />
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                pathname === route.href
+                  ? 'text-white bg-white/10'
+                  : 'text-zinc-400'
+              )}
+            >
+              <div className='flex items-center flex-1'>
+                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
                 {route.label}
               </div>
             </Link>
