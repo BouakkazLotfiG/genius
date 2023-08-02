b034fce091b3a1beeb591bd951c6517e6ae8f43e
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-08-02 10:48:48 +0100

    fixed api limit in mobile menu bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
+import { getApiLimitCount } from '@/lib/api-limit';
 import { UserButton } from '@clerk/nextjs';
 
 import React from 'react';
 import MobileSidebar from './MobileSidebar';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
   return (
     <div className='flex items-center p-4'>
-      <MobileSidebar />
+      <MobileSidebar apiLimitCount={apiLimitCount} />
       <div className='flex w-full justify-end '>
         <UserButton afterSignOutUrl='/' />
       </div>
