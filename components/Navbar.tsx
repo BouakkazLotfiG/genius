595885b2fe6c8718b75cedb4fedb1b99f0029517
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-08-02 15:06:20 +0100

    fix prop

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import MobileSidebar from './MobileSidebar';
 
 const Navbar = async () => {
   const apiLimitCount = await getApiLimitCount();
-  const isPro = checkSubscription();
+  const isPro = await checkSubscription();
   return (
     <div className='flex items-center p-4'>
       <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
