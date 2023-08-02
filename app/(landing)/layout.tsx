a38f5c99d5eae11ba5e05987c3761531b97732ee
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-08-02 14:23:14 +0100

    fix import

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,6 @@
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='h-full  overflow-auto'>
+    <main className='h-full landing overflow-auto'>
       <div className='mx-auto max-w-screen-xl h-full w-full'>{children}</div>
     </main>
   );
